d37b19980b94a9156262e95d18a3db38e0db24c0
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2024-04-19 18:06:39 -0300

    Desafio Concluído

---
+++ Models/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿namespace desafio_projeto_hospedagem.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+        public Pessoa() { }
+
+        public Pessoa(string nome)
+        {
+            Nome = nome;
+        }
+        
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ( "Hello, World!" );
+﻿using desafio_projeto_hospedagem.Models;
+using System.Globalization;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo ( "pt-BR" );
+
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1", sobrenome: "Hospedado 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2", sobrenome: "Hospedado 2");
+
+hospedes.Add ( p1 );
+hospedes.Add ( p2 );
+
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 3, valorDiaria: 30);
+
+
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite ( suite );
+reserva.CadastrarHospedes ( hospedes );
+
+
+Console.WriteLine ( $"Hóspedes: {reserva.ObterQuantidadeHospedes ( )}" );
+Console.WriteLine ( $"Valor diária: {reserva.CalcularValorDiaria ( ).ToString ( "C", CultureInfo.CreateSpecificCulture ( "pt-BR" ) )}" );
